351c223a10d7543b9484c1bf488ec9ed8ae6c662
r-vanooyen <robin.vanooyen@gmail.com>
2022-05-19 20:57:26 +0200

    feat: implement api 1.0.0 #3
    created spring boot application with api dependency

---
+++ src/test/java/open/timetrack/backend/springboot/BackendApplicationTest.java
@@ -0,0 +1,17 @@
+package open.timetrack.backend.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class BackendApplicationTest {
+
+    void contextLoads() {
+    }
+
+    @Test
+    void starts() {
+        assertTrue(true);
+    }
+}
